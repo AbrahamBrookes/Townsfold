932b97035c4953807035ea463da907706cc3ab0c
Abraham Brookes <abrahambrookes@gmail.com>
2021-12-05 22:00:23 +0000

    started on action menu

---
+++ resources/js/layout/ActionMenu.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="action-menu">
+        <div class="action-menu-header">
+            {{ current_intro }}
+        </div>
+        <div class="action-menu__item" v-for="item in items" :key="item.id">
+            <a :href="item.href" :class="item.class" :title="item.title">
+                <i :class="item.icon"></i>
+                <span>{{ item.text }}</span>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        current_intro() {
+            return 'asdasdasd'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
